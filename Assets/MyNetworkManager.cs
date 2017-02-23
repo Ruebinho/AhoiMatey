6967c059513052f7c39585e65d78dc5324cb03ed
Dennis Rübner <dennis.ruebner@gmail.com>
2017-02-23 17:23:59 +0100

    Network connection logging added

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MyStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Starting Host");
+        StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Host started");
+    }
+
+    public override void OnStartClient(NetworkClient myNetClient)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Client start requested" );
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        //base.OnClientConnect(conn);
+        Debug.Log(Time.timeSinceLevelLoad + " Client connected to IP: " + conn.address);
+    }
+}
