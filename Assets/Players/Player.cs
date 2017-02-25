a801da6f9e266d1e1660acd408ba5bdbedfbefd3
Dennis Rübner <dennis.ruebner@gmail.com>
2017-02-25 14:35:32 +0100

    Section finished Player camera fixes

--- Assets/Players/Player.cs
+++ Assets/Players/Player.cs
@@ -27,4 +27,10 @@ public class Player : NetworkBehaviour {
 
         transform.Translate(inputValue);
     }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponentInChildren<Camera>().enabled = true;
+
+    }
 }
